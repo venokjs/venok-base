27ce0e49f37ea7ed2287dbb10771ba0278feb94b
0x303133 <0x303133@proton.me>
2023-08-02 02:18:36 +0300

    feat: Create testing config and @swc/jest support

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  moduleFileExtensions: ["js", "json", "ts"],
+  rootDir: './',
+  modulePaths: ['<rootDir>'],
+  testRegex: ".spec.ts$",
+  collectCoverageFrom: ["**/*.(t|j)s"],
+  coverageDirectory: "../coverage",
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.(t|j)sx?$": "@swc/jest",
+  },
+};
